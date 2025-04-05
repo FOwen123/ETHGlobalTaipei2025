38f0eb901dd280d4d9dba4fdc00ec435b1ff3ad0
nielderto <nielderto@gmail.com>
2025-04-05 09:39:02 +0800

    added wallet connector

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
@@ -8,9 +9,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center text-center w-full min-h-screen">
         <h1 className="text-4xl font-bold">Project 1</h1>
         <p className="text-lg text-gray-400">Make your transactions in web3 easier, by using a prompt.</p>
-        <div>
-
-        </div>
+        <ConnectButton />
       </div>
     </>
 
